a93c4cfa05d1cfd279d4ae058239d9b166edf3e5
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-08-25 11:01:08 -0700

    adding notes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sameTime
-Given a list of peoples' birth year and death year, find the year(s) in which the most people were alive at the same time.
+ - Given a list of peoples' birth year and death year, find the year(s) in which the most people were alive at the same time.
+
+ - ex: [[1910, 1950], [1900, 1951], [1945, 2000]] would return [1945, 1946, 1947, 1948, 1949, 1950]
+
+### Installation
+- npm install
+
+### Run Test
+- npm test
--- sameTime.js
+++ sameTime.js
@@ -1,27 +1,41 @@
+
 exports.sameTime = (arr) => {
+  // Checking for if input parameter is an not array or length is
+  // less than zero. If true will return zero.
   if (!Array.isArray(arr) || arr.length <= 0) {
     return [];
   }
   
+  // Checking for if array doesn't contain another array inside the current 
+  // array. And lenght of current array is equal to lenght of 2.
+  // If true push current values into element 0 of array. And pop last element.
   if (!Array.isArray(arr[0]) && arr.length === 2) {
      arr[0] = [arr[0], arr[1]];
      arr.pop();
   }
   
   var timeLine = [];
+
+  // Adding first value for comparison
   var same = [arr[0][0], arr[0][1]];
 
   for (let i = 1; i < arr.length; i++) {
       var time = arr[i];
 
+      // Checking for current time is greater than same
+      // ex time = 1990 and same is 1989 (1990 > 1989) true
       if (time[0] > same[0]) {
           same[0] = time[0];
       }
 
+      // Checking for current time is greater than same
+      // ex time = 1989 and same is 1990 (1989 < 1990) true
       if (time[1] < same[1]) {
           same[1] = time[1];
       }
   }
+  // At the end of for loop same variable should equal sameTime values 
+  // of people who live durning the sameTime
   
   for (let i = same[0], j = 0; i <= same[1]; i++, j++) {
        timeLine[j] = i;
